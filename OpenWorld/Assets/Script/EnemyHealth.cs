33db75d8be648dc58e0c1081c9e2dafcf90f288d
djohnico1987 <49573447+djohnico1987@users.noreply.github.com>
2019-07-12 11:38:44 -0500

    test

---
+++ OpenWorld/Assets/Script/EnemyHealth.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public EnemyParameters enemyVariables;
+
+    private int currentHealth;
+    private bool invurnerable = false;
+    private bool ifVurn = true; //keeps from being restarted on each frame
+    
+    //sets enemy's health at start
+    private void Start()
+    {
+        currentHealth = enemyVariables.health;
+    }
+
+    private void Update()
+    {
+        if(currentHealth == 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    //works to toggle invurnerable state
+    private void LateUpdate()
+    {
+        if (invurnerable && ifVurn)
+        {
+            ifVurn = false;
+            StartCoroutine(Damageable());
+        }
+    }
+    
+    private void OnCollisionStay(Collision collision)
+    {
+        if (invurnerable == false && collision.gameObject.CompareTag("PlayerDamage"))
+        {
+            currentHealth--;
+            invurnerable = true;
+        }
+    }
+
+    //toggles invurnerablity on timer
+    IEnumerator Damageable()
+    {
+        yield return new WaitForSeconds(enemyVariables.invurnerableTimer);
+        invurnerable = false;
+        ifVurn = true;
+    }
+}
